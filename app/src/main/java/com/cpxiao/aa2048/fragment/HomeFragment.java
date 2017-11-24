38debefced864f4868b3c47e499f6c183912ae54
cpxiao <xchp2008@126.com>
2017-11-24 13:59:12 +0800

    gp109(1.0.9), add rate and share button.

--- app/src/main/java/com/cpxiao/aa2048/fragment/HomeFragment.java
+++ app/src/main/java/com/cpxiao/aa2048/fragment/HomeFragment.java
@@ -6,10 +6,14 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.cpxiao.R;
 import com.cpxiao.aa2048.mode.extra.Extra;
 import com.cpxiao.androidutils.library.utils.PreferencesUtils;
+import com.cpxiao.androidutils.library.utils.RateAppUtils;
+import com.cpxiao.androidutils.library.utils.ShareAppUtils;
 import com.cpxiao.gamelib.fragment.BaseZAdsFragment;
 
 /**
@@ -33,20 +37,21 @@ public class HomeFragment extends BaseZAdsFragment implements View.OnClickListen
         Button hard = (Button) view.findViewById(R.id.hard);
         Button insane = (Button) view.findViewById(R.id.insane);
         Button impossible = (Button) view.findViewById(R.id.impossible);
-        Button bestScore = (Button) view.findViewById(R.id.best_score);
-        Button settings = (Button) view.findViewById(R.id.settings);
-        Button quit = (Button) view.findViewById(R.id.quit);
+        ImageButton rateApp = (ImageButton) view.findViewById(R.id.rate_app);
+        ImageButton share = (ImageButton) view.findViewById(R.id.share);
+        ImageButton bestScore = (ImageButton) view.findViewById(R.id.best_score);
+        ImageButton settings = (ImageButton) view.findViewById(R.id.settings);
 
         easy.setOnClickListener(this);
         normal.setOnClickListener(this);
         hard.setOnClickListener(this);
         insane.setOnClickListener(this);
         impossible.setOnClickListener(this);
+        rateApp.setOnClickListener(this);
+        share.setOnClickListener(this);
         bestScore.setOnClickListener(this);
         settings.setOnClickListener(this);
-        quit.setOnClickListener(this);
 
-        settings.setVisibility(View.GONE);
     }
 
     @Override
@@ -79,12 +84,17 @@ public class HomeFragment extends BaseZAdsFragment implements View.OnClickListen
             bundle.putInt(Extra.Key.SQUARE_COUNT_X, 8);
             bundle.putInt(Extra.Key.SQUARE_COUNT_Y, 8);
             addFragment(GameFragment.newInstance(bundle));
+        } else if (id == R.id.rate_app) {
+            RateAppUtils.rate(context);
+        } else if (id == R.id.share) {
+            String msg = getString(R.string.share_msg) + "\n" +
+                    getString(R.string.app_name) + "\n" +
+                    "https://play.google.com/store/apps/details?id=" + context.getPackageName();
+            ShareAppUtils.share(context, getString(R.string.share), msg);
         } else if (id == R.id.best_score) {
             showBestScoreDialog(context);
         } else if (id == R.id.settings) {
-
-        } else if (id == R.id.quit) {
-            removeFragment();
+            Toast.makeText(context, "Coming soon...", Toast.LENGTH_SHORT).show();
         }
     }
 
